f7788757f82f057478d7b70481b7a72e219b19c7
HandyGold75 <IZO@HandyGold75.com>
2025-01-08 15:49:31 +0100

    08-Jan-25

--- GroupManagement/GroupManagement.go
+++ GroupManagement/GroupManagement.go
@@ -1,11 +1,50 @@
 package GroupManagement
 
+import (
+	"encoding/xml"
+	"strconv"
+)
+
 type (
 	GroupManagement struct {
 		Send func(action, body, targetTag string) (string, error)
+	}
+
+	addMemberResponse struct {
+		XMLName                  xml.Name `xml:"AddMemberResponse"`
+		CurrentTransportSettings string
+		CurrentURI               string
+		GroupUUIDJoined          string
+		ResetVolumeAfter         bool
+		VolumeAVTransportURI     string
 	}
 )
 
 func New(send func(action, body, targetTag string) (string, error)) GroupManagement {
 	return GroupManagement{Send: send}
+}
+
+func (s *GroupManagement) AddMember(memberID string, bootSeq int) (addMemberResponse, error) {
+	res, err := s.Send("AddMember", "<MemberID>"+memberID+"</MemberID><BootSeq>"+strconv.Itoa(bootSeq)+"</BootSeq>", "s:Body")
+	if err != nil {
+		return addMemberResponse{}, err
+	}
+	data := addMemberResponse{}
+	err = xml.Unmarshal([]byte(res), &data)
+	return data, err
+}
+
+func (s *GroupManagement) RemoveMember(memberID string) error {
+	_, err := s.Send("RemoveMember", "<MemberID>"+memberID+"</MemberID>", "")
+	return err
+}
+
+func (s *GroupManagement) ReportTrackBufferingResult(memberID string, resultCode int) error {
+	_, err := s.Send("ReportTrackBufferingResult", "<MemberID>"+memberID+"</MemberID><ResultCode>"+strconv.Itoa(resultCode)+"</ResultCode>", "")
+	return err
+}
+
+func (s *GroupManagement) SetSourceAreaIds(desiredSourceAreaIds string) error {
+	_, err := s.Send("SetSourceAreaIds", "<DesiredSourceAreaIds>"+desiredSourceAreaIds+"</DesiredSourceAreaIds>", "")
+	return err
 }
--- Helper/GroupManagement.go
+++ Helper/GroupManagement.go
@@ -1,1 +1,11 @@
 package Helper
+
+// Short for `zp.ConnectionManager.GetCurrentConnectionIDs`.
+func (h *Helper) AddMember(memberID string, bootSeq int) (string, error) {
+	out, err := h.groupManagement.AddMember(memberID, bootSeq)
+	return out.CurrentURI, err
+}
+
+func (h *Helper) RemoveMember(memberID string) error {
+	return h.groupManagement.RemoveMember(memberID)
+}
