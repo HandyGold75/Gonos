51c8f9b0d3156207efcd5ba46a1dd685efde8bc7
HandyGold75 <Ian.AnMaZo@gmail.com>
2025-01-17 16:30:58 +0100

    17-Jan-25

--- Helper/MusicServices.go
+++ Helper/MusicServices.go
@@ -1,1 +1,22 @@
 package Helper
+
+type AvailableServices struct {
+	DescriptorList string
+	TypeList       string
+	ListVersion    string
+}
+
+// Short for `zp.MusicServices.ListAvailableServices'`.
+func (h *Helper) ListAvailableServices() (AvailableServices, error) {
+	res, err := h.musicServices.ListAvailableServices()
+	return AvailableServices{
+		DescriptorList: res.AvailableServiceDescriptorList,
+		TypeList:       res.AvailableServiceTypeList,
+		ListVersion:    res.AvailableServiceListVersion,
+	}, err
+}
+
+// Short for `zp.MusicServices.UpdateAvailableServices'`.
+func (h *Helper) UpdateAvailableServices() error {
+	return h.musicServices.UpdateAvailableServices()
+}
--- MusicServices/MusicServices.go
+++ MusicServices/MusicServices.go
@@ -1,11 +1,42 @@
 package MusicServices
 
+import (
+	"encoding/xml"
+	"strconv"
+)
+
 type (
 	MusicServices struct {
 		Send func(action, body, targetTag string) (string, error)
+	}
+
+	listAvailableServicesResponse struct {
+		XMLName                        xml.Name `xml:"ListAvailableServicesResponse"`
+		AvailableServiceDescriptorList string
+		AvailableServiceTypeList       string
+		AvailableServiceListVersion    string
 	}
 )
 
 func New(send func(action, body, targetTag string) (string, error)) MusicServices {
 	return MusicServices{Send: send}
+}
+
+func (s *MusicServices) GetSessionId(serviceId int, username string) (string, error) {
+	return s.Send("GetSessionId", "<ServiceId>"+strconv.Itoa(serviceId)+"</ServiceId><Username>"+username+"</Username>", "SessionId")
+}
+
+func (s *MusicServices) ListAvailableServices() (listAvailableServicesResponse, error) {
+	res, err := s.Send("ListAvailableServices", "", "s:Body")
+	if err != nil {
+		return listAvailableServicesResponse{}, err
+	}
+	data := listAvailableServicesResponse{}
+	err = xml.Unmarshal([]byte(res), &data)
+	return data, err
+}
+
+func (s *MusicServices) UpdateAvailableServices() error {
+	_, err := s.Send("UpdateAvailableServices", "", "")
+	return err
 }
