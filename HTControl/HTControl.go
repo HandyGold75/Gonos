dd63edc8c9684879927384360665a5a0b0a6f90d
HandyGold75 <IZO@HandyGold75.com>
2025-01-17 16:00:01 +0100

    17-Jan-25

--- HTControl/HTControl.go
+++ HTControl/HTControl.go
@@ -1,5 +1,10 @@
 package HTControl
 
+import (
+	"Gonos/lib"
+	"strconv"
+)
+
 type (
 	HTControl struct {
 		Send func(action, body, targetTag string) (string, error)
@@ -8,4 +13,44 @@ type (
 
 func New(send func(action, body, targetTag string) (string, error)) HTControl {
 	return HTControl{Send: send}
+}
+
+func (s *HTControl) CommitLearnedIRCodes(name string) error {
+	_, err := s.Send("CommitLearnedIRCodes", "<Name>"+name+"</Name>", "")
+	return err
+}
+
+func (s *HTControl) GetIRRepeaterState() (bool, error) {
+	res, err := s.Send("GetIRRepeaterState", "", "CurrentIRRepeaterState")
+	return res == "On", err
+}
+
+func (s *HTControl) GetLEDFeedbackState() (bool, error) {
+	res, err := s.Send("GetLEDFeedbackState", "", "LEDFeedbackState")
+	return res == "On", err
+}
+
+func (s *HTControl) IdentifyIRRemote(timeout int) error {
+	_, err := s.Send("IdentifyIRRemote", "<Timeout>"+strconv.Itoa(timeout)+"</Timeout>", "")
+	return err
+}
+
+func (s *HTControl) IsRemoteConfigured(timeout int) (bool, error) {
+	res, err := s.Send("IsRemoteConfigured", "", "RemoteConfigured")
+	return res == "1", err
+}
+
+func (s *HTControl) LearnIRCode(iRCode string, timeout int) error {
+	_, err := s.Send("LearnIRCode", "<IRCode>"+iRCode+"</IRCode><Timeout>"+strconv.Itoa(timeout)+"</Timeout>", "")
+	return err
+}
+
+func (s *HTControl) SetIRRepeaterState(state bool) error {
+	_, err := s.Send("SetIRRepeaterState", "<DesiredIRRepeaterState>"+lib.BoolToOnOff(state)+"</DesiredIRRepeaterState>", "")
+	return err
+}
+
+func (s *HTControl) SetLEDFeedbackState(state bool) error {
+	_, err := s.Send("SetLEDFeedbackState", "<LEDFeedbackState>"+lib.BoolToOnOff(state)+"</LEDFeedbackState>", "")
+	return err
 }
--- Helper/HTControl.go
+++ Helper/HTControl.go
@@ -1,1 +1,9 @@
 package Helper
+
+func (h *Helper) GetIRRepeaterState() (bool, error) {
+	return h.hTControl.GetIRRepeaterState()
+}
+
+func (h *Helper) SetLEDFeedbackState(state bool) error {
+	return h.hTControl.SetLEDFeedbackState(state)
+}
