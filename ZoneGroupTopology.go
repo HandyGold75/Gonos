77cb792885f912bd9f5287ed8189d35fba3078a1
HandyGold75-OC <OC@HandyGold75.com>
2026-05-08 14:47:27 +0200

    docs: fix Go doc comments in ZoneGroupTopology helpers
    Rewrites comments to start with the function name, matching the Go
    documentation convention so they are picked up by godoc.

--- ZoneGroupTopology.go
+++ ZoneGroupTopology.go
@@ -1,24 +1,24 @@
 package Gonos
 
-// Get ZoneGroupAttribute ZoneGroupName
+// GetZoneGroupName returns the current zone group name.
 func (zp *ZonePlayer) GetZoneGroupName() (string, error) {
 	res, err := zp.ZoneGroupTopology.GetZoneGroupAttributes()
 	return res.CurrentZoneGroupName, err
 }
 
-// Get ZoneGroupAttribute ZoneGroupID
+// GetZoneGroupID returns the current zone group ID.
 func (zp *ZonePlayer) GetZoneGroupID() (string, error) {
 	res, err := zp.ZoneGroupTopology.GetZoneGroupAttributes()
 	return res.CurrentZoneGroupID, err
 }
 
-// Get ZoneGroupAttribute ZonePlayerUUIDsInGroup
+// GetZonePlayerUUIDsInGroup returns the UUIDs of all zone players in the current group.
 func (zp *ZonePlayer) GetZonePlayerUUIDsInGroup() (string, error) {
 	res, err := zp.ZoneGroupTopology.GetZoneGroupAttributes()
 	return res.CurrentZonePlayerUUIDsInGroup, err
 }
 
-// Get ZoneGroupAttribute MuseHouseholdId
+// GetMuseHouseholdId returns the Muse household ID.
 func (zp *ZonePlayer) GetMuseHouseholdId() (string, error) {
 	res, err := zp.ZoneGroupTopology.GetZoneGroupAttributes()
 	return res.CurrentMuseHouseholdId, err
